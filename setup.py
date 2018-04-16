05ee109a53c575a26dedb7dab7781e08552f9376
ecerquei <eduardomcerqueira@gmail.com>
2018-04-16 14:30:24 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='restock',
+    author="Kai Munechika, Helen Zhang",
+    packages=['restock',
+              'restock.controller',
+              'restock.model',
+              'restock.util',
+              'restock.db'],
+    include_package_data=True,
+    install_requires=['flask',
+                      'requests',
+                      'pymongo',
+                      'queuelib'],
+)
