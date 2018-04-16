dbd74535349c2686b56e6b898d1342aa6e3ca553
Helen Zhang <hz96@bu.edu>
2018-04-16 12:33:32 -0400

    updated the profile page

--- static/index.js
+++ static/index.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     // all custom jQuery will go here
     $('.dropdown-trigger').dropdown();
     $('select').formSelect();
+    $('.tabs').tabs();
     var form = document.getElementById("user_search");
     document.getElementById("pressed_search").addEventListener("click", function () {
         form.submit();
